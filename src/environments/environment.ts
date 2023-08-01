ee9aada68d9766722524829070648514e8635928
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-08-01 15:07:46 +0200

    Added firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'simple-crm-a6663',
+    appId: '1:22805702227:web:49303d878d0aebf70348a6',
+    storageBucket: 'simple-crm-a6663.appspot.com',
+    apiKey: 'AIzaSyDpSmnxvvMoPRKnRKJvc7rRMVfcCbuZe4I',
+    authDomain: 'simple-crm-a6663.firebaseapp.com',
+    messagingSenderId: '22805702227',
+  },
+};
